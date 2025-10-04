e4af8a9ad58bea656f7467f0a9fa89ab145fbd65
IAN OTOLLO <ianotollo@gmail.com>
2025-10-04 18:23:55 +0300

    editing client.ts

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -1,8 +1,6 @@
-// src/integrations/client.ts
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL!;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY!;
 
-// This is your main Supabase client
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
